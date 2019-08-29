10aab7c80ee741820526a3daaa818382ac795cfa
vincentsarago <vincent.sarago@gmail.com>
2019-08-30 01:06:38 +0300

    better readme and example

--- examples/package/package.sh
+++ examples/package/package.sh
@@ -3,21 +3,18 @@ echo "-----------------------"
 echo "Creating lambda package"
 echo "-----------------------"
 echo "Remove lambda python packages"
-rm -rdf $PREFIX/python/boto3/ \
-  && rm -rdf $PREFIX/python/botocore/ \
-  && rm -rdf $PREFIX/python/docutils/ \
-  && rm -rdf $PREFIX/python/dateutil/ \
-  && rm -rdf $PREFIX/python/jmespath/ \
-  && rm -rdf $PREFIX/python/s3transfer/ \
-  && rm -rdf $PREFIX/python/numpy/doc/
+rm -rdf $PACKAGE_PREFIX/boto3/ \
+  && rm -rdf $PACKAGE_PREFIX/botocore/ \
+  && rm -rdf $PACKAGE_PREFIX/docutils/ \
+  && rm -rdf $PACKAGE_PREFIX/dateutil/ \
+  && rm -rdf $PACKAGE_PREFIX/jmespath/ \
+  && rm -rdf $PACKAGE_PREFIX/s3transfer/ \
+  && rm -rdf $PACKAGE_PREFIX/numpy/doc/
 
 echo "Strip shared libraries"
 cd $PREFIX && find lib -name \*.so\* -exec strip {} \;
 
 echo "Create archive"
-cd $PREFIX/python && zip -r9q /tmp/package.zip *
-cd $PREFIX && zip -r9q --symlinks /tmp/package.zip lib/*.so*
-cd $PREFIX && zip -r9q /tmp/package.zip share
-cd $PREFIX && zip -r9q /tmp/package.zip bin
-
+cd $PACKAGE_PREFIX && zip -r9q /tmp/package.zip *
+cd $PREFIX && zip -r9q --symlinks /tmp/package.zip lib/*.so* share bin
 cp /tmp/package.zip /local/package.zip
